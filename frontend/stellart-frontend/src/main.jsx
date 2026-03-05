40873b7dd608b2298bf393701c6adada21a0ca0f
Alberto-jhen <114157250+amorar05@users.noreply.github.com>
2026-03-05 22:23:39 +0100

    Frontend initialized.
    Created frontend folder with Vite + React and added tailwindcss dependencie.
    Main structure configured with BrowserRouter and Routes.
    TODO: Landing page.

---
+++ frontend/stellart-frontend/src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
+
+
+const root = document.getElementById('root');
+if (root) {
+  ReactDOM.createRoot(root).render (
+    <>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </>
+  )
+}
