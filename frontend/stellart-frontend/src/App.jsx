2826da1a2789d5497ea38b104857b3bc4eb284e1
Alberto-jhen <114157250+amorar05@users.noreply.github.com>
2026-03-06 17:10:39 +0100

    Feature: Layout.
    Implemented default component layout for better cohesion throughout the app.

--- frontend/stellart-frontend/src/App.jsx
+++ frontend/stellart-frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
 import Landing from './pages/Landing';
+import { Layout } from './components/layout/Layout';
 
 export default function App() {
 	return (
 		<Routes>
-			<Route path="/" element={<Landing />} />
+			<Route path="/" element={<Layout> <Landing /> </Layout>} />
 		</Routes>
 	)
 }
