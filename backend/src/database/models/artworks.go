dfd2d63542af2bb5dcc0b98fd9b9f764ef6c1c8d
Alberto-jhen <amorar05@estudiantes.unileon.es>
2026-03-31 21:44:33 +0200

    Price attribute fix.
    Fix: Added price as attribute in the repository and the supabase table as well.

--- backend/src/database/models/artworks.go
+++ backend/src/database/models/artworks.go
@@ -4,11 +4,11 @@ import "time"
 
 type Artwork struct {
 	ID          string    `json:"id"`
-	ArtistID    string    `json:"artist_id"`
+	ArtistID    *string   `json:"artist_id"`
 	Title       string    `json:"title"`
 	Description *string   `json:"description"`
 	ImageURL    string    `json:"image_url"`
-	Price       float64   `json:"price"`
+	Price       *float64  `json:"price"`
 	Tags        []string  `json:"tags"`
 	Embedding   []float32 `json:"embedding"`
 	CreatedAt   time.Time `json:"created_at"`
