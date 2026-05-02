943781e9a53887246f684d4f463ca2bfa58ee6b9
Alberto-jhen <114157250+amorar05@users.noreply.github.com>
2026-05-02 19:18:59 +0200

    Add: Playwright
    Added new e2e folder and playwright installation from scracth, including 1 test for landing page as an example to follow later on.

--- e2e/tests/stellart.spec.js
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('La landing page de Stellart carga correctamente', async ({ page }) => {
-  await page.goto('http://localhost:5173/');
-  await expect(page.locator('#root')).toBeVisible();
-});
