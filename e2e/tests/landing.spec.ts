7e397f3159fa6fa71fa473cc821217416e7e765f
Alberto-jhen <amorar05@estudiantes.unileon.es>
2026-05-30 12:52:26 +0200

    Update: e2e tests
    Updated landing tests, still work to do.

--- e2e/tests/landing.spec.ts
+++ e2e/tests/landing.spec.ts
@@ -1,25 +1,33 @@
 import { test, expect } from '@playwright/test';
 
-test.describe('Landing Page de Stellart', () => {
-  
-  test('debería cargar la página principal y mostrar el título correcto', async ({ page }) => {
-    await page.goto('/');
+test.describe('Stellart Landing Page', () => {
 
-    // Comprueba que el texto "Bring out the star" es visible.
-    await expect(page.getByText('Bring out the star')).toBeVisible();
-    
-    // Comprueba que el texto "inside of you" es visible.
-    await expect(page.getByText('inside of you')).toBeVisible();
-  });
+	// INFO: Always do page.goto() before testing.
+	test('should load the main page and show the correct title', async ({ page }) => {
+		await page.goto('/');
 
-  test('el botón "Explore gallery" debería navegar a la ruta /explore', async ({ page }) => {
-    await page.goto('/');
+		// Checks that the text "Bring out the star" is visible.
+		await expect(page.getByText('Bring out the star')).toBeVisible();
+		
+		// Checks that the text "inside of you" is visible.
+		await expect(page.getByText('inside of you')).toBeVisible();
+	});
 
-    // Busca el botón "Explore gallery" y hace click.
-    await page.getByRole('button', { name: 'Explore gallery' }).click();
+	test('the "Explore gallery" button should navigate to the /explore route', async ({ page }) => {
+		await page.goto('/');
 
-    // Comprobamos que tras hacer click la url en la que estamos contiene la ruta "/explore".
-    await expect(page).toHaveURL(/.*\/explore/);
-  });
+		// Finds the "Explore gallery" button and clicks it.
+		await page.getByRole('button', { name: 'Explore gallery' }).click();
 
+		// Verifies that after clicking, the current URL contains the "/explore" route.
+		await expect(page).toHaveURL(/.*\/explore/);
+	});
+
+	test('the "Join For Free" button should navigate to the /register route', async ({ page }) => {
+		await page.goto('/');
+
+		await page.getByRole('button', {name: 'Join For Free' }).click();
+
+		await expect(page).toHaveURL(/.*\/register/);
+	});
 });
